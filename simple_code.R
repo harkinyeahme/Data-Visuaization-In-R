5a50c3cab5788b6edc151c18203ad38057efbc3f
Akinyemi Olanrewaju Akinwale <akinyemiolanrewaju14@gmail.com>
2026-01-24 08:19:08 +0100

    Add files via upload

---
+++ simple_code.R
@@ -0,0 +1,12 @@
+name <- "Akinyemi Olanrewaju Akinwale"
+role <- "Bionformatics Intern"
+affiliation <- "Hackbio"
+gene <- "wbeo"
+organism <- "Vibrio cholerae"
+serogroup <- "01 serogroup"
+
+print(paste0("My name is ", name,". ",
+       "I am a ", role, " at ", affiliation, ".
+       ", "My favourite gene is ", gene,
+       " that can be found in the ", serogroup, " of ", organism))
+
